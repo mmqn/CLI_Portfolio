c73c931c558365add2621bc32f159f152ec9b36e
mmqn <mmq.newgyn@gmail.com>
2019-02-11 20:34:20 -0800

    Updates

---
+++ precache-manifest.269a63fe4b0668157d0591cad6fabb12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "de2dd9339ae7636475fcd91b3ed0e24f",
+    "url": "/cli_portfolio/static/media/Poppins-Regular.de2dd933.otf"
+  },
+  {
+    "revision": "1a728ab67ea9383bdb7ceb425d373ede",
+    "url": "/cli_portfolio/static/media/Anonymous-Pro.1a728ab6.ttf"
+  },
+  {
+    "revision": "1d171b5399fc5e28f3bb72d7dd959d7f",
+    "url": "/cli_portfolio/static/media/Anonymous-Pro-Bold.1d171b53.ttf"
+  },
+  {
+    "revision": "c7e4d39dac81c0d04489",
+    "url": "/cli_portfolio/static/js/runtime~main.c7e4d39d.js"
+  },
+  {
+    "revision": "4d014a9677ef41fff7f5",
+    "url": "/cli_portfolio/static/js/main.4d014a96.chunk.js"
+  },
+  {
+    "revision": "c7e4760d26caaa4fdfab",
+    "url": "/cli_portfolio/static/js/2.c7e4760d.chunk.js"
+  },
+  {
+    "revision": "4d014a9677ef41fff7f5",
+    "url": "/cli_portfolio/static/css/main.afd8634f.chunk.css"
+  },
+  {
+    "revision": "e7e99607446e9990c19ffb0cb799cfe4",
+    "url": "/cli_portfolio/index.html"
+  }
+];
